b6fd53e7e13c1b7820e8526f1cf73d9d829f8e07
Italo <italo.ufs@gmail.com>
2012-11-06 18:28:20 -0300

    Adicionado Enum TipoPecas

--- src/Componentes/PecasDecorator.java
+++ src/Componentes/PecasDecorator.java
@@ -1,9 +1,19 @@
 package Componentes;
 
+import Enum.TipoPeca;
+
 public abstract class PecasDecorator extends Gabinete{
 	
-	
+	private TipoPeca tipoPeca;
 	
 	public abstract String getDescricao();
+
+	public void setTipoPeca(TipoPeca tipoPeca) {
+		this.tipoPeca = tipoPeca;
+	}
+
+	public TipoPeca getTipoPeca() {
+		return tipoPeca;
+	}
 
 }
--- src/Enum/TipoPeca.java
+++ src/Enum/TipoPeca.java
@@ -1,5 +1,22 @@
 package Enum;
 
 public enum TipoPeca {
+		
+	PLACA_MAE("Placa-mãe"),
+    MEMORIA("Memória"),
+    HD("HD"),
+    GRAVADOR("Gravador"),
+	PLACA_VIDEO("Placa de vídeo");
+    
+    private String nome;
 
+    private TipoPeca(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+	
 }

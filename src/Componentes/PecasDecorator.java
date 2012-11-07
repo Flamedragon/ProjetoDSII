86c3cbec3845829deef8129b75e8fd23c380ebbe
Italo <italo.ufs@gmail.com>
2012-11-07 15:56:52 -0300

    MOD PecasDecorator para possuir Marca e listaMarca

--- src/Componentes/PecasDecorator.java
+++ src/Componentes/PecasDecorator.java
@@ -1,11 +1,16 @@
 package Componentes;
 
+import java.util.List;
+
+import Enum.Marca;
 import Enum.TipoPeca;
 
 public abstract class PecasDecorator extends Gabinete{
-	
+	  
 	protected TipoPeca tipoPeca;
 	protected Gabinete gab;
+	protected Marca marca;
+	protected List<Marca> listaMarcas;
 	
 	public abstract String getDescricao();
 
@@ -16,5 +21,23 @@ public abstract class PecasDecorator extends Gabinete{
 	public TipoPeca getTipoPeca() {
 		return tipoPeca;
 	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public List<Marca> getListaMarcas() {
+		return listaMarcas;
+	}
+
+	public void setListaMarcas(List<Marca> listaMarcas) {
+		this.listaMarcas = listaMarcas;
+	}
+	
+	
 
 }

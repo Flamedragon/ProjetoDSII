f7d96ce544ed390acbf07f85dd1a5c2855eb63c9
rfsbarreto <rfsbarreto@gmail.com>
2012-11-06 23:47:13 -0300

    Repositorio organizado

--- src/Componentes/Placa_Mae_Asus.java
+++ src/Componentes/Placa_Mae_Asus.java
@@ -1,39 +1,4 @@
-<<<<<<< HEAD
-package Componentes;
 
-import java.util.List;
-
-public class Placa_Mae_Asus extends PecasDecorator {
-
-	String tipo = "Placa Mãe";
-	
-	public Placa_Mae_Asus(Gabinete g) {
-
-		//for (List<PecasDecorator> g.componentes : comp) {
-			
-//		}
-		
-		g.componentes.add(this);
-		descricao= g.descricao + "c/ placa mãe Asus";
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public String getDescricao() {
-		// TODO Auto-generated method stub
-		return this.descricao;
-	}
-
-
-	@Override
-	public float getCusto() {
-		// TODO Auto-generated method stub
-		return 100;
-	}
-
-
-}
-=======
 package Componentes;
 
 import Enum.TipoPeca;
@@ -47,24 +12,22 @@ public class Placa_Mae_Asus extends PecasDecorator {
 	
 	public Placa_Mae_Asus(Gabinete g) {
 		tipoPeca = TipoPeca.PLACA_MAE;
-		descricao= g.descricao + "c/ placa m�e Asus";
-		// TODO Auto-generated constructor stub
+		descricao= g.descricao + "c/ placa m�e Asus";
 	}
 
 	@Override
 	public String getDescricao() {
-		// TODO Auto-generated method stub
+
 		return this.descricao;
 	}
 
 
 	@Override
 	public float getCusto() {
-		// TODO Auto-generated method stub
 		return 100;
 	}
 
 
 
 }
->>>>>>> 143cf1796ba85a6912edef851398fac762891db4
+

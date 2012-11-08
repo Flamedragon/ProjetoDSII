18f5aad57bc9d4769561ec4c41213e55464489ab
rfsbarreto <rfsbarreto@gmail.com>
2012-11-07 22:34:30 -0300

    Gab Clone e Generico

---
+++ src/Componentes/GabineteGenerico.java
@@ -0,0 +1,15 @@
+package Componentes;
+
+
+public class GabineteGenerico extends Gabinete {
+	
+	public String getDescricao() {
+		return "Gabinete Genérico";
+	}
+	
+	public float getCusto() {
+
+		return 70;
+	}
+	
+}
